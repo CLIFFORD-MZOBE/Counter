65c8f615935d8eddb12b6bc0ca332ea5967897b1
siphiwe <cliffordscmzobe@gmail.com>
2018-06-27 09:50:30 +0100

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,46 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+value1: number;
+value2: number;
+
   constructor(public navCtrl: NavController) {
-
+    
+ this.value1 = 0;
+ this.value2 = 0; 
   }
-
+  onClick(x){
+    switch(x) { 
+      case '1': { 
+       this.value1 +=1;
+         break; 
+      } 
+      case "2": { 
+        this.value1 +=2; 
+         break; 
+      } 
+      case "3": {
+        this.value1 +=3; 
+         break;    
+      } 
+      case "4": { 
+        this.value2 +=1; 
+         break; 
+      }  
+      case "5": { 
+        this.value2 +=2; 
+        break; 
+     } 
+     case "6": { 
+      this.value2 +=3; 
+      break; 
+      }
+      case "reset": { 
+        this.value1 = 0;
+        this.value2 = 0;
+        break;  
+      }
+   }
+  }
+ 
 }
